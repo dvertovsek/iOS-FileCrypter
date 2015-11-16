df872b7ef4c3141bd0490bef2b076a4ae46c358d
Darijan <dvertovs@foi.hr>
2015-11-16 09:17:26 +0100

    Initial Commit

---
+++ iOS-FileCrypter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS-FileCrypter
+//
+//  Created by MTLab on 16/11/15.
+//  Copyright © 2015 air. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
